bad52a59fa2f1f549afba286e824c75f5a024db3
fguedez <fguedez1311@gmail.com>
2022-06-02 07:35:12 -0400

    Version 0.0.2

--- codigofacilitofran/__main__.py
+++ codigofacilitofran/__main__.py
@@ -3,17 +3,12 @@ import logging
 from codigofacilitofran import unreleased
 
 logging.basicConfig(level=logging.INFO)
+def main():
+    logging.info(unreleased())
+
 
 if __name__=='__main__':
-    """
-        INFO ->10
-        DEBUG ->20
-        WARNING ->30
-        ERROR   ->40
-        CRITICAL ->50
-
-    """
+    
     logging.debug("Estamos comenzando la ejecución del paquete")
-    # workshops=unreleased()
-    logging.debug(help(unreleased))
+    main()
     logging.debug("Estamos finalizando la ejecución del paquete")
