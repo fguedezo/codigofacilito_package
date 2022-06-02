1c7bb6ce78b5975821d1a53d9eb81205991acd78
fguedez <fguedez1311@gmail.com>
2022-06-02 07:58:06 -0400

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Este paquete permite consumir API'
 PACKAGE_NAME = 'codigofacilitofran'
 AUTHOR = 'Francisco Guedez'
